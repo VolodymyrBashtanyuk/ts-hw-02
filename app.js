62e1bc6632bafafdc1984ea1a7779170b053ad3a
VOLODYMYR BASHTANYUK <96777965+VolodymyrBashtanyuk@users.noreply.github.com>
2023-01-20 23:35:33 +0200

    add settings and first task

---
+++ app.js
@@ -0,0 +1,13 @@
+var age;
+var names;
+var toggle;
+var empty;
+var notInitialize;
+var callback;
+age = 50;
+names = 'Max';
+toggle = true;
+empty = null;
+notInitialize = undefined;
+callback = function (a) { return 100 + a; };
+console.log('let age: number, let names: string, let toggle: boolean, let empty: null ,let notInitialize: undefined, let callback: (num: number) => void');
---
+++ app.ts
@@ -0,0 +1,16 @@
+
+let age: number;
+let names: string;
+let toggle: boolean;
+let empty: null;
+let notInitialize: undefined;
+let callback: (num: number) => void; 
+
+ age = 50;
+ names = 'Max';
+ toggle = true;
+ empty = null;
+ notInitialize = undefined;
+callback = (a) => { return 100 + a };
+ 
+console.log('let age: number, let names: string, let toggle: boolean, let empty: null ,let notInitialize: undefined, let callback: (num: number) => void');
